03119a4439fcdef4b5f685873071f2d3787ed1d8
Louis Thibault <l.thibault@sentimens.com>
2021-05-31 22:29:10 -0400

    Add examples/basic

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"io"
+	"log"
+	"strings"
 
+	"github.com/libp2p/go-libp2p-core/network"
 	mx "github.com/wetware/matrix/pkg"
 	"github.com/wetware/matrix/pkg/net"
 )
 
-const ns = "matrix.example.basic"
-
-func opDiscover() mx.OpFunc {
-	return mx.Announce(net.SelectAll{}, ns).
-		Then(mx.Discover(net.SelectAll{}, ns))
-}
+const (
+	ns   = "matrix.example.basic"
+	echo = "/echo"
+)
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
@@ -30,11 +33,49 @@ func main() {
 	h0 := sim.MustHost(ctx)
 	h1 := sim.MustHost(ctx)
 
+	h0 = sim.MustHost(ctx)
+	h0.SetStreamHandler(echo, handler)
+
 	/*
 	 Matrix provides the Operations API, which allows developers
 	 to compose operations on collections of hosts.
+
+	 Here, we're using a simple two-stage pipeline to announce
+	 each peer to the namespace and connect them to each other.
 	*/
-	sim.Op(opDiscover()).
+	sim.Op(mx.Announce(net.SelectAll{}, ns)).
+		Then(mx.Discover(net.SelectAll{}, ns)).
 		Call(ctx, h0, h1).
 		Must()
+
+	s, err := h1.NewStream(ctx, h0.ID(), echo)
+	maybeFatal(err)
+
+	/*
+	 Now we open a stream from h1 to h0, write some data, read
+	 the response, and log it to stdout.
+	*/
+
+	var buf bytes.Buffer
+	_, err = io.Copy(s, strings.NewReader("Hello, world!"))
+	maybeFatal(err)
+	maybeFatal(s.CloseWrite())
+
+	_, err = io.Copy(&buf, s)
+	maybeFatal(err)
+
+	log.Println(buf.String())
+}
+
+func maybeFatal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handler(s network.Stream) {
+	defer s.Close()
+
+	_, err := io.Copy(s, s)
+	maybeFatal(err)
 }
