1f3ca0418b9b4c4d73007703835812cfd54fb949
Louis Thibault <l.thibault@sentimens.com>
2021-08-02 16:58:32 -0400

    Don't override user options in HostFactory.

--- pkg/netsim/netsim.go
+++ pkg/netsim/netsim.go
@@ -65,10 +65,10 @@ func (f *HostFactory) NewHost(ctx context.Context, opt []config.Option) (host.Ho
 }
 
 func (f *HostFactory) hostopt(opt []libp2p.Option) []config.Option {
-	return append(opt,
-		// override options that users may have passed
+	return append([]libp2p.Option{
 		libp2p.NoListenAddrs,
 		libp2p.ListenAddrStrings("/inproc/~"),
 		libp2p.NoTransports,
-		libp2p.Transport(inproc.New(inproc.WithEnv(f))))
+		libp2p.Transport(inproc.New(inproc.WithEnv(f))),
+	}, opt...)
 }
